21f682ffe11b762d64c6c2e8b352def74ca4f5a4
Nurlan Mukhambetov <nurlanm@Nurlans-MBP.hsd1.il.comcast.net>
2023-11-06 14:56:32 -0600

    added changes

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello word");
 
+        String str = "hello batch 3 students" ;
+        System.out.println(str);
+
 
     }
 }
